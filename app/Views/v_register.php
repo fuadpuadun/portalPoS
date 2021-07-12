611848bd68dccea619c6c6186007729cdc831264
fuad sheri <fuadpuadun@gmail.com>
2021-07-12 17:39:25 +0700

    Commit Sore
    sisa register belum beres, konfirm password sama perataan text belum bener

--- app/Views/v_register.php
+++ app/Views/v_register.php
@@ -1,57 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>portalPoS - Login</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<style>
-.login-form {
-    width: 340px;
-    margin: 50px auto;
-  	font-size: 15px;
-}
-.login-form form {
-    margin-bottom: 15px;
-    background: #f7f7f7;
-    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-    padding: 30px;
-}
-.login-form h2 {
-    margin: 0 0 15px;
-}
-.form-control, .btn {
-    min-height: 38px;
-    border-radius: 2px;
-}
-.btn {        
-    font-size: 15px;
-    color: #FFFFFF;
-    <!--font-weight: bold;-->
-}
-</style>
-</head>
-<body>
-<div class="login-form">
-    <form action="/examples/actions/confirmation.php" method="post">
-        <h2 class="text-center">Login</h2>       
-        <div class="form-group">
-            <input type="text" class="form-control" placeholder="Username" required="required">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>portalPoS - Login</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        
+        <style>
+            .login-form {
+                width: 600px;
+                margin: 50px auto;
+  	            font-size: 15px;
+            }
+            .login-form form {
+                margin-bottom: 15px;
+                background: #f7f7f7;
+                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+                padding: 30px;
+            }
+            .login-form h2 {
+                margin: 0 0 15px;
+            }
+            .form-control, .btn {
+                width: 400px;
+                min-height: 40px;
+                border-radius: 2px;
+            }
+            .btn {        
+                font-size: 15px;
+                background-color:#58DD55; 
+                color: #FFFFFF;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="login-form">
+            <form action="/examples/actions/confirmation.php" method="post">
+                <h2 class="text-center">Register</h2>
+                <!--Isian-->      
+                <div class="form-group row">
+                <label for="staticNamaUMKM" class="col-sm-3 col-form-label">Nama UMKM</label>
+                    <input type="text" class="form-control" placeholder="Nama UMKM" required="required">
+                </div>
+                <div class="form-group row">
+                <label for="staticAlamat" class="col-sm-3 col-form-label">Alamat</label>
+                    <input type="text" class="form-control" placeholder="Alamat" required="required">
+                </div>
+                <div class="form-group row">
+                <label for="staticNoHp" class="col-sm-3 col-form-label">No Hp</label>
+                    <input type="text" class="form-control" placeholder="No Hp" required="required">
+                </div>
+                <div class="form-group row">
+                <label for="email" class="col-sm-3 col-form-label">Email</label>
+                    <input type="text" class="form-control" placeholder="Email" required="required">
+                </div>
+                <!--pw-->
+                <div class="form-group row">
+                <label for="pw" class="col-sm-3 col-form-label">Password</label>
+                    <input type="password" class="form-control" placeholder="Password" required="required">
+                </div>
+                <div class="form-group row">
+                <label for="konfirmPw" class="col-sm-3 col-form-label">Konfirmasi Password</label>
+                    <input type="password" class="form-control" placeholder="Konfirmasi Password" required="required">
+                </div>
+                <!--Tombol Register-->
+                <div class="form-group" >
+                    <button type="submit" class="btn  btn-block float-right">Register</button>
+                </div>
+                <div class="clearfix">
+                    <p class="text-center">Sudah punya akun ? <a href="//renol">masuk</a> sekarang</p>
+                </div>        
+            </form>
         </div>
-        <div class="form-group">
-            <input type="password" class="form-control" placeholder="Password" required="required">
-        </div>
-        <div class="form-group">
-            <button type="submit" class="btn  btn-block" style="background-color: #58DD55 !important;">Login</button>
-        </div>
-        <div class="clearfix">
-            <p class="text-center">Belum punya akun ? <a href="//renol">buat akun</a> sekarang</p>
-        </div>        
-    </form>
-</div>
-</body>
+    </body>
 </html>
--- app/Views/v_template.php
+++ app/Views/v_template.php
@@ -9,10 +9,16 @@
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+        <style>
+            .navbar {
+                background-color: #2D58C7;
+            }
+        </style>
     </head>
     <body>
         <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top shadow" style="background-color: #2D58C7 !important;">
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top shadow">
             <div class="container-fluid">
                 <a class="navbar-brand font-weight-bold" href="<?php echo base_url('beranda'); ?>">
                     <img src="<?= base_url('web/logo.png') ?>" width="50" class="img-thumbnail">
