a09d7fc597acd88117b3436a8083b7e5e3679f6c
Renol P. H <reshifr@gmail.com>
2021-08-05 12:28:21 +0700

    fix: penamaan method

--- app/Models/m_item.php
+++ app/Models/m_item.php
@@ -14,16 +14,36 @@ class m_item extends Model {
         $this->database = db_connect();
 	}
 
-    public function getItems(string $keyword = null) {
+    public function getExistItems(string $keyword = null) {
         $auth = $this->signin->getAuth();
         $idUmkm = $auth['idUmkm'];
         if( $keyword==null )
             $sql = "SELECT nama_barang, harga_barang, stok_barang
                     FROM barang
                     WHERE id_umkm = '$idUmkm'
+                    AND stok_barang > 0
                     ORDER BY nama_barang ASC";
         else
             $sql = "SELECT nama_barang, harga_barang, stok_barang
+                    FROM barang
+                    WHERE id_umkm = '$idUmkm'
+                    AND stok_barang > 0
+                    AND nama_barang LIKE '%$keyword%'
+                    ORDER BY nama_barang ASC";
+        $result = $this->database->query($sql);
+        return $result->getResultArray();
+    }
+    
+    public function getItems(string $keyword = null) {
+        $auth = $this->signin->getAuth();
+        $idUmkm = $auth['idUmkm'];
+        if( $keyword==null )
+            $sql = "SELECT nama_barang, harga_barang, stok_barang, stok_minimal
+                    FROM barang
+                    WHERE id_umkm = '$idUmkm'
+                    ORDER BY nama_barang ASC";
+        else
+            $sql = "SELECT nama_barang, harga_barang, stok_barang, stok_minimal
                     FROM barang
                     WHERE id_umkm = '$idUmkm'
                     AND nama_barang LIKE '%$keyword%'
