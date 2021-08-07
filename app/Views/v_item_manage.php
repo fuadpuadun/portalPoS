55da4ea75b99c19b1ceed1839bff9acad2df34b2
fuad sheri <fuadpuadun@gmail.com>
2021-08-07 11:32:56 +0700

    Perbaikan Delete

--- app/Views/v_item_manage.php
+++ app/Views/v_item_manage.php
@@ -39,36 +39,8 @@
                 <td><button class="btn btn-primary" style=" border:none; background-color:#676767 !important;" data-record-id="54" data-record-title="Something cool" data-toggle="modal" data-target="#confirm-delete">Ubah <i class="far fa-edit" ></i></button></td>
                 <!-- Trigger Delete -->
                 <td>
-                    <button type="button" class="btn btn-danger btn-sm btn-delete" data-toggle="modal" style=" border:none; background-color:#FF0000 !important;" data-target="#deleteModal">
-                    Hapus <i class="far fa-trash-alt" ></i>
-                    </button>
+                    <a href="#" class="btn btn-danger btn-sm btn-delete" data-id="<?= $barang['nama_barang'];?>">Delete</a>
                 </td>
-
-                <!-- modal delete -->
-                <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="deleteModalLongTitle">Hapus</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <form action="<?= base_url('manage/delete') ?>" method="post" id="delete">
-                                    Anda akan menghapus data barang. Tindakan ini tidak dapat dikembalikan.
-
-                                    Lanjutkan ?
-                                    <input type="hidden" name="nama_barang" id="nama_barang" value="<?php echo $itemName; ?>">
-                                </form>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal" style=" border:none; background-color:#FF0000;">Batalkan</button>
-                                <button form="delete" type="submit" class="btn btn-primary" >Lanjutkan</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </tr>
             <?php } ?>
         </tbody>
@@ -99,6 +71,48 @@
         </tfoot>
     </table>
 </div>
+
+<!-- modal delete -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLongTitle">Hapus</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= base_url('manage/delete') ?>" method="post" id="delete">
+                                    Anda akan menghapus data barang. Tindakan ini tidak dapat dikembalikan.
+
+                                    Lanjutkan ?
+                    <input type="hidden" name="nama_barang" class="nama_barang">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal" style=" border:none; background-color:#FF0000;">Batalkan</button>
+                <button form="delete" type="submit" class="btn btn-primary" >Lanjutkan</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function(){
+
+        // get Delete Product
+        $('.btn-delete').on('click',function(){
+            // get data from button edit
+            const id = $(this).data('id');
+            // Set data to Form Edit
+            $('.nama_barang').val(id);
+            // Call Modal Delete
+            $('#deleteModal').modal('show');
+        });
+        
+    });
+</script>
 
 <?= $this->endSection() ?>
 </html>
